27fe70c10d550c0e26e3178bf63ce4bdcb515ee3
Alexis Bravo <alexis.ve@gmail.com>
2017-08-10 00:45:31 +0200

    WhatTheFuckAssignment example added to chapter2

---
+++ chapter2/WhatTheFuckAssignment.java
@@ -0,0 +1,18 @@
+class WhatTheFuckAssignment {
+	public static void main(String... args) {
+
+		//Crazy shit
+		long x=5, y = (x=3);
+		System.out.println("X=" + x);
+		System.out.println("Y=" + y);
+
+		//Java and its things :/
+		int ___ = (___ = 5) ;
+		System.out.println("___="+___);
+	
+		//Da fack!
+		int $$$ = ($$$ = ($$$ = ($$$ = 9)) ) ;
+		System.out.println("$$$="+$$$);
+	
+	}
+}
