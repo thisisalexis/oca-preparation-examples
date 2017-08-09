8e61208a2e4705d9def21ce8170e5b5f63a0da22
Alexis Bravo <alexis.ve@gmail.com>
2017-08-09 22:46:58 +0200

    Casting example added

---
+++ chapter2/CastingExamples.java
@@ -0,0 +1,15 @@
+class CastingExamples {
+	public static void main(String[] argumentos) {
+		int $ = (int) 256.0; //Casting from double to int
+		int _var = (byte) 99999999999999L;
+
+		//--
+		short _5 = 5;
+		short __8__$$$$ = 10;
+		short $_1_5 = (short) (_5 * __8__$$$$);
+
+		System.out.println($);
+		System.out.println(_var);
+		System.out.println($_1_5);
+	}
+}
