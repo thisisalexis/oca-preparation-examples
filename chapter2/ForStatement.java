223b8b4a5e86b41c2db027c98d1294130f6c16ef
Alexis Bravo <alexis.ve@gmail.com>
2017-08-11 00:14:49 -0430

    ForStatement.java and .class added to the project

---
+++ chapter2/ForStatement.java
@@ -0,0 +1,10 @@
+public class ForStatement {
+	public static void main(String... argumentos) {
+		for(int x = 0; x <= 9; x++){
+			System.out.println(x);
+		}
+		int  anInt = 0;
+		for(String aString = "hola"; anInt <= 9; anInt++)
+			System.out.println(aString);
+	}
+}
