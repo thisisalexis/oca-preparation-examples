4e82d96ccb4efeb74261a0e7fd7cb13b4113b202
Alexis Bravo <alexis.ve@gmail.com>
2017-08-18 16:08:07 -0430

    indexOf example added to StringMethods

--- chapter3/StringMethods.java
+++ chapter3/StringMethods.java
@@ -1,12 +1,15 @@
 public class StringMethods {
 	public static void main(String... args) {
 
-		//Ejemplo 1
+		//Ejemplo charAt()
 		StringMethods.testingCharAt();
+		
+		//Ejemplo indexOf
+		StringMethods.testingIndexOf();
 	}
 
 	public static void testingCharAt(){
-				String aString = "animals";
+		String aString = "animals";
 
 		System.out.println(aString.charAt(0));		//a
 		System.out.println(aString.charAt(1));		//n
@@ -15,7 +18,21 @@ public class StringMethods {
 		System.out.println(aString.charAt(4));		//a
 		System.out.println(aString.charAt(5));		//l
 		System.out.println(aString.charAt(6));		//s
-		System.out.println(aString.charAt(7));		//Excepción en TIEMPO DE EJCUCIÖN (SI COMPILA) porque está fuera de rango
+		//System.out.println(aString.charAt(7));		//Excepción en TIEMPO DE EJCUCIÖN (SI COMPILA) porque está fuera de rango
+	}
+
+	//Ejemplo de variantes de indexOf
+	public static void testingIndexOf(){
+		String name = "animnals";
+
+		System.out.println( ">>>>>>>>>>>>EJEMPLO indexOf >>>>>>>>>>>>>>>>>" );
+		
+		System.out.println( name.indexOf('a') );	//0
+		System.out.println( name.indexOf("al") );	//5
+		System.out.println( name.indexOf('a', 4) );	//5
+		System.out.println( name.indexOf("al", 6) );//-1 NO LO ENCUENTRA
+				
+			
 	}
 
 }
