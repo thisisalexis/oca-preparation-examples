0421fa3c9459f0ef50d06c7b16f6f8e1c3c6090c
Alexis Bravo <alexis.ve@gmail.com>
2017-08-17 22:32:41 -0430

    IfThenElseStatement.java edited to add new examples and scenarios

--- chapter2/IfThenElseStatement.java
+++ chapter2/IfThenElseStatement.java
@@ -13,5 +13,32 @@ class IfThenElseStatement {
 		} else 
 			System.out.println("Good Evening");
 
+		//Call another example
+		IfThenElseStatement.trickyMethod();
+		IfThenElseStatement.trickyMethodTwo();
+	}
+
+	//Anocher very important example
+	public static void trickyMethod() {
+		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>> \nEjemplo dos: trickyMethod.");
+		
+		boolean result = true;
+		
+		if(result = false) //ESTO SI HACE LA ASIGNACION!!!! Convierne result a false y la expresión devuelve false, por lo que entra en el else
+			System.out.println("Result sigue siendo true: " + result);
+		else
+			System.out.println("Result ahora es false: " + result);
+	}
+
+	//Este ejemplo es un complemento de trickyMethod()
+	public static void trickyMethodTwo() {
+		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>> \nEjemplo dos: trickyMethod.");
+		
+		int result = 0;
+		
+		if(result = 1) ///ERRORRRRRR...No compila, porque la asignación devuelve un entero y no un booleano
+			System.out.println(result);
+		else
+			System.out.println(result);
 	}
 }
